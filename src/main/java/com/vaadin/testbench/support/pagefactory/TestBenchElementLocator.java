8642af82ad4a611330166343184abb5adba8a5dc
mouellet <mat_ouel@hotmail.com>
2014-11-14 09:48:13 -0500

    ElementQuery generation based on annotation attributes

--- src/main/java/com/vaadin/testbench/support/pagefactory/TestBenchElementLocator.java
+++ src/main/java/com/vaadin/testbench/support/pagefactory/TestBenchElementLocator.java
@@ -99,7 +99,6 @@ public class TestBenchElementLocator implements ElementLocator {
             }
         }
 
-        System.err.println(elementQuery.generateQuery());
         return elementQuery;
     }
 
